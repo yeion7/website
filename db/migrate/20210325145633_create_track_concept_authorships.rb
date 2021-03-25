56c7d22593b23900857caa82124baa9179f0d63f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-25 16:14:49 +0100

    Create table for concept authorship/contributorship

---
+++ db/migrate/20210325145633_create_track_concept_authorships.rb
@@ -0,0 +1,12 @@
+class CreateTrackConceptAuthorships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :track_concept_authorships do |t|
+      t.belongs_to :track_concept, foreign_key: true, null: false
+      t.belongs_to :user, foreign_key: true, null: false
+
+      t.timestamps
+
+      t.index %i[track_concept_id user_id], name: "index_concept_authorship_on_track_concept_id_and_user_id", unique: true
+    end
+  end
+end
